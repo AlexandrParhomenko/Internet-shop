fb10e73c9f61fbb4a6f244e7f6d30d48a5d78197
AlexandrParhomenko <sania_38@list.ru>
2023-03-26 02:32:32 +0600

    feat: add filter

--- internet-shop/src/components/footer/Footer.tsx
+++ internet-shop/src/components/footer/Footer.tsx
@@ -5,19 +5,57 @@ const Footer = () => {
       <div className='footer'>
         <div className='footer__logo-block'>
           <img src='/sultanwhite.png' height='66' width='156' alt='sultan'/>
-          <span className='footer__text'>Компания «Султан» — снабжаем розничные магазины товарами "под ключ" в Кокчетаве и Акмолинской области</span>
-          <span>Подпишись на скидки и акции</span>
-          <div>
-            <span>Введите ваш E-mail</span>
-            <div></div>
+          <span style={{paddingTop: '15px', paddingBottom: '45px'}} className='footer__text'>Компания «Султан» — снабжаем розничные магазины товарами<br/> "под ключ" в Кокчетаве и Акмолинской области</span>
+          <span style={{paddingBottom: '15px'}} className='footer__text small'>Подпишись на скидки и акции</span>
+          <div className='email-wrapper'>
+            <div>Введите ваш E-mail</div>
+            <div className='arrow-wrapper'></div>
           </div>
         </div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className='menu-wrapper'>
+          <span className='general-font'>Меню сайта:</span>
+          <span className='footer__text medium'>О компании</span>
+          <span className='footer__text medium'>Доставка и оплата</span>
+          <span className='footer__text medium'>Возврат</span>
+          <span className='footer__text medium'>Контакты</span>
         </div>
+        <div className='menu-wrapper'>
+          <span className='general-font'>Категории:</span>
+          <span className='footer__text medium'>Бытовая химия</span>
+          <span className='footer__text medium'>Косметика и гигиена</span>
+          <span className='footer__text medium'>Товары для дома</span>
+          <span className='footer__text medium'>Товары для детей и мам</span>
+          <span className='footer__text medium'>Посуда</span>
+        </div>
+        <div className='menu-wrapper'>
+          <span className='general-font'>Скачать прайс-лист:</span>
+          <div className='yellow-btn'>
+            <span className='footer__text'>Прайс-лист</span>
+            <img src='/bx_bxs-download.png' alt='frame'/>
+          </div>
+          <span className='footer__text medium'>Связь в мессенджерах:</span>
+          <div className='social-wrapper'>
+            <img src='/Group%20162.png' height='39' width='39' alt='social'/>
+            <img src='/logos_telegram.png' height='39' width='39' alt='social'/>
+          </div>
+        </div>
+        <div className='contact-wrapper'>
+          <span className='general-font'>Контакты:</span>
+          <div className='contact-wrapper__block'>
+            <span className='general-font medium'>+7 (777) 490-00-91</span>
+            <span className='footer__text small'>время работы: 9:00-20:00</span>
+            <span className='footer__text underline'>Заказать звонок</span>
+          </div>
+          <div className='contact-wrapper__mail'>
+            <span className='general-font medium'>opt.sultan@mail.ru</span>
+            <span className='footer__text small'>На связи в любое время</span>
+          </div>
+          <div className='social-wrapper'>
+            <img src='/Visa.png' height='39' width='61' alt='social'/>
+            <img src='/Visa%20(1).png' height='39' width='61' alt='social'/>
+          </div>
+        </div>
+
       </div>
   );
 };
--- internet-shop/src/components/main/Main.tsx
+++ internet-shop/src/components/main/Main.tsx
@@ -1,6 +1,124 @@
-import React from 'react';
+import React, {useState} from 'react';
+import data from '../../data.json';
+
 
 const Main = () => {
+  const [showFilter, setShowFilter] = useState<boolean>(false)
+  const [minPrice, setMinPrice] = useState<number>(0)
+  const [maxPrice, setMaxPrice] = useState<number>(10000)
+  const [manuSearch, setManuSearch] = useState<string>('')
+  const [manuFilter] = useState<string[]>([])
+  const [typeFilter, setTypeFilter] = useState<string[]>([])
+  const headersArray: string[] = [
+    'Уход за руками',
+    'Уход за ногами',
+    'Уход за лицом',
+    'Уход за волосами',
+    'Средства для загара',
+    'Средства для бритья',
+    'Подарочные наборы',
+    'Гигиеническая продукция',
+    'Гигиена полости рта',
+    'Бумажная продукция'
+  ]
+
+  const getManufacturers = () => {
+    let arr: string[] = data.products.items.map(el => el.manufacturer)
+    return Array.from(new Set(arr))
+  }
+
+  const checkboxHandler = (el: string) => {
+    manuFilter.indexOf(el) === -1 ? manuFilter.push(el) : manuFilter.splice(manuFilter.indexOf(el), 1)
+  }
+
+  const removeFilters = () => {
+    setMinPrice(0)
+    setMaxPrice(10000)
+    setManuSearch('')
+    console.log(typeFilter)
+  }
+
+  const addActive = (el: string) => {
+    return typeFilter.indexOf(el) !== -1 ? 'sort-types__item weight-font name-font active' : 'sort-types__item weight-font name-font'
+  }
+
+  const activeHandler = (e: string) => {
+    typeFilter.indexOf(e) === -1 ? setTypeFilter([...typeFilter, e]) : console.log(typeFilter.splice(typeFilter.indexOf(e), 1))
+    console.log(typeFilter)
+  }
+  const addActive2 = (el: string) => {return typeFilter.indexOf(el) !== -1 ? 'aside-filters__header active' : 'aside-filters__header'}
+
+  const searchHandle = () => {
+    return manuSearch.length !== 0 ?
+        getManufacturers().map(el => {
+          if (el.toLowerCase().includes(manuSearch.toLowerCase()))
+            return <div className='checkbox-wrapper'>
+              <input className={el} onChange={() => checkboxHandler(el)} type='checkbox'/>
+              <span className='weight-font name-font'>{el}</span>
+            </div>
+        }) : getManufacturers().map(el =>
+            <div className='checkbox-wrapper'>
+              <input onChange={() => checkboxHandler(el)} type='checkbox'/>
+              <span className='weight-font name-font'>{el}</span>
+            </div>)
+  }
+
+
+  const filterHandle = () => {
+    return showFilter ? data.products.items.map(el => {
+      if (parseInt(el.price.split(' ')[0]) > minPrice && parseInt(el.price.split(' ')[0]) < maxPrice) {
+        return <div className='shop-items__container__item'>
+          <div className='img-wrapper'>
+            <img src={`/${el.img}.png`} alt='img'/>
+          </div>
+          <div></div>
+          <span className='weight-font'>{el.weight}</span>
+          <span className='main-text'>{el.name}</span>
+          <div>
+            <span className='weight-font name-font'>Штрихкод: </span>
+            <span className='description-font'>{el.code}</span>
+          </div>
+          <div>
+            <span className='weight-font name-font'>Производитель: </span>
+            <span className='description-font'>{el.manufacturer}</span>
+          </div>
+          <div>
+            <span className='weight-font name-font'>Брэнд: </span>
+            <span className='description-font'>{el.brand}</span>
+          </div>
+          <div className='price-wrapper'>
+            <span className='main-text price-font'>{el.price}</span>
+            <div className='cart-btn'>в корзину</div>
+          </div>
+        </div>
+      }
+    }) : data.products.items.map(el =>
+        <div className='shop-items__container__item'>
+          <div className='img-wrapper'>
+            <img src={`/${el.img}.png`} alt='img'/>
+          </div>
+          <div></div>
+          <span className='weight-font'>{el.weight}</span>
+          <span className='main-text'>{el.name}</span>
+          <div>
+            <span className='weight-font name-font'>Штрихкод: </span>
+            <span className='description-font'>{el.code}</span>
+          </div>
+          <div>
+            <span className='weight-font name-font'>Производитель: </span>
+            <span className='description-font'>{el.manufacturer}</span>
+          </div>
+          <div>
+            <span className='weight-font name-font'>Брэнд: </span>
+            <span className='description-font'>{el.brand}</span>
+          </div>
+          <div className='price-wrapper'>
+            <span className='main-text price-font'>{el.price}</span>
+            <div className='cart-btn'>в корзину</div>
+          </div>
+        </div>)
+  }
+
   return (
       <div className='main'>
         <div className='main__nav'>
@@ -21,17 +139,7 @@ const Main = () => {
             </div>
           </div>
           <div className='sort-types'>
-            <div className='sort-types__item'>Уход за телом</div>
-            <div className='sort-types__item'>Уход за руками</div>
-            <div className='sort-types__item'>Уход за ногами</div>
-            <div className='sort-types__item'>Уход за лицом</div>
-            <div className='sort-types__item'>Уход за волосами</div>
-            <div className='sort-types__item'>Средства для загара</div>
-            <div className='sort-types__item'>Средства для бритья</div>
-            <div className='sort-types__item'>Подарочные наборы</div>
-            <div className='sort-types__item'>Гигиеническая продукция</div>
-            <div className='sort-types__item'>Гигиена полости рта</div>
-            <div className='sort-types__item'>Бумажная продукция</div>
+            {headersArray.map(e => <div onClick={() => activeHandler(e)} className={addActive(e)}>{e}</div>)}
           </div>
           <div className='shop-items'>
             <div className='aside-filters'>
@@ -39,39 +147,37 @@ const Main = () => {
                 <span className='aside-filters__title'>ПОДБОР ПО ПАРАМЕТРАМ</span>
                 <span>Цена ₸</span>
                 <div className='input-wrapper'>
-                  <input value='0' className='input' type='text'/>
+                  <input value={minPrice}
+                         onChange={(e) => setMinPrice(parseInt(e.target.value) || 0)}
+                         className='input' type='text'/>
                   <span>-</span>
-                  <input value='10000' className='input' type='text'/>
+                  <input value={maxPrice}
+                         onChange={(e) => setMaxPrice(parseInt(e.target.value) || 0)}
+                         className='input' type='text'/>
                 </div>
               </div>
               <div>
                 <span className='aside-filters__title'>Производитель</span>
                 <div style={{margin: 0}} className='search-bar'>
-                  <input type='text' placeholder='Поиск...' className='search-bar__text search-input'/>
+                  <input type='text' value={manuSearch}
+                         onChange={(e) => setManuSearch(e.target.value)} placeholder='Поиск...'
+                         className='search-bar__text search-input'/>
                   <div className='search-logo'></div>
                 </div>
-                <div>
-                  <div>
-                    <input type='checkbox'/>
-                    <span className=''>Manufacturer (58)</span>
-                  </div>
+                <div className='checkbox-container'>
+                  {searchHandle()}
                 </div>
               </div>
-              <div className='aside-filters__header'>Уход за телом</div>
-              <div className='aside-filters__header'>Уход за руками</div>
-              <div className='aside-filters__header'>Уход за ногами</div>
-              <div className='aside-filters__header'>Уход за лицом</div>
-              <div className='aside-filters__header'>Уход за волосами</div>
-              <div className='aside-filters__header'>Средства для загара</div>
-              <div className='aside-filters__header'>Средства для бритья</div>
-              <div className='aside-filters__header'>Подарочные наборы</div>
-              <div className='aside-filters__header'>Гигиеническая продукция</div>
-              <div className='aside-filters__header'>Гигиена полости рта</div>
-              <div className='aside-filters__header'>Бумажная продукция</div>
+              <div className='filters-block'>
+                <div onClick={() => setShowFilter(true)} className='yellow-btn'>
+                  <span className='footer__text'>Показать</span>
+                </div>
+                <div onClick={() => removeFilters()} className='delete-block'></div>
+              </div>
+              {headersArray.map(e => <div onClick={() => typeFilter.push(e)} className={addActive2(e)}>{e}</div>)}
             </div>
             <div className='shop-items__container'>
-              <div className='shop-items__container__item'>
-              </div>
+              {filterHandle()}
             </div>
           </div>
         </div>
